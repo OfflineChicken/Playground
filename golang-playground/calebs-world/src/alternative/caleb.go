752a2fdcd737972e166b829257f3ec9eab1e0d63
Caleb Piekstra <cpiekstra@newrelic.com>
2020-04-23 17:40:40 -0700

    Adding calebs special world

---
+++ golang-playground/calebs-world/src/alternative/caleb.go
@@ -0,0 +1,14 @@
+// Package alternative is the best alternative to packages
+// Click here and start typing.
+package alternative
+
+import "fmt"
+
+func notVisible() {
+	fmt.Println("Hello, invisible Caleb")
+}
+
+// Visible function
+func Visible() {
+	fmt.Println("Hello, visible Caleb")
+}
---
+++ golang-playground/calebs-world/src/go-world.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"alternative"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+	alternative.Visible()
+}
